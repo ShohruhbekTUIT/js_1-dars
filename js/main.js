a4d69c8448c903c51437cd1558071666ded61e7c
ShohruhbekTUIT <96619217+ShohruhbekTUIT@users.noreply.github.com>
2022-02-19 11:13:14 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var firstName = prompt("Ismingizni kiriting");
+var savolOne = prompt("Margin orqali o'rtaga keltirishda qaysi classdan foydalaniladi ?");
+var savolTwo = prompt("Inputni classi nima ?");
+var savolThree = prompt("col larni o'rab turuvchi class nomi nima ?");
+var savolFour = prompt("Columnni eng kattasi classi nomi nima ?");
+var savolFive = prompt("Selectning classi nima ?");
+var savolSix = prompt("Orqa fonni och-ko'k qiladigan class nomi ?");
+var savolSeven = prompt("Buttonni orqasi qizil rangda qiladigan class ?");
+var savolEight = prompt("Bir vaqtning o'zida tepa va pastdan padding berish uchun qaysi classdan foydalanamiz ?");
+var savolNine = prompt("Range ning classi nima ?");
+var savolTen = prompt("Card dagi rasmning classi nima ?");
+  
+console.log( firstName + "\n" +
+savolOne + "\n" +
+savolTwo + "\n" +
+savolThree + "\n" +
+savolFour + "\n" +
+savolFive + "\n" +
+savolSix + "\n" +
+savolSeven + "\n" +
+savolEight + "\n" +
+savolNine + "\n" +
+savolTen
+);
